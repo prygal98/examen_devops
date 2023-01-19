0c011dcba5f03d91fc85a6a85099fb6ec2cb81d7
Raphael Malka <raphael.malka@student.vinci.be>
2023-01-19 19:57:24 +0100

    Changes test

--- src/__tests__/wordTable.test.js
+++ src/__tests__/wordTable.test.js
@@ -1,6 +1,6 @@
 const { deleteWordWithX } = require("../wordTable");
 
-describe("First test to check if the function return an empty table", () => {
+describe("First test to check if the function return an empty table CHANGES", () => {
     test("should return an empty table", () => {
         const result = deleteWordWithX([]);
         expect(result).toEqual([]);
