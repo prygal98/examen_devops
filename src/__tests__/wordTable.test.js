2e57476c9222f5cfafc056e749fcd50abc4eae9d
Raphael Malka <raphael.malka@student.vinci.be>
2023-01-19 19:30:24 +0100

    change in ci.yml

--- src/__tests__/wordTable.test.js
+++ src/__tests__/wordTable.test.js
@@ -1,6 +1,5 @@
 const { deleteWordWithX } = require("../wordTable");
 
-
 describe("First test to check if the function return an empty table", () => {
     test("should return an empty table", () => {
         const result = deleteWordWithX([]);
@@ -8,28 +7,26 @@ describe("First test to check if the function return an empty table", () => {
     });
 });
 
-
 describe("Second test to check if the function return the correct values ", () => {
     test("should return a table with the 3 elements", () => {
-        const result = deleteWordWithX(['Momo', 'Hadji', 'Leo']);
-        expect(result).toEqual(['Momo', 'Hadji', 'Leo']);
+        const result = deleteWordWithX(["Momo", "Hadji", "Leo"]);
+        expect(result).toEqual(["Momo", "Hadji", "Leo"]);
         expect(result).toHaveLength(3);
     });
 });
 
-
 describe("Third test to check if the function return the correct values ", () => {
     test("should return a table with the 2 elements", () => {
-        const result = deleteWordWithX(['DMX', 'Xzibit', 'JeanKevin']);
-        expect(result).toEqual(['JeanKevin']);
+        const result = deleteWordWithX(["DMX", "Xzibit", "JeanKevin"]);
+        expect(result).toEqual(["JeanKevin"]);
         expect(result).toHaveLength(1);
     });
 });
 
 describe("Fourth test to check if the function return the correct values", () => {
     test("should return a table with the 1 element", () => {
-        const result = deleteWordWithX(['JC', 'Leo', 'xena']);
-        expect(result).toEqual(['JC','Leo']);
+        const result = deleteWordWithX(["JC", "Leo", "xena"]);
+        expect(result).toEqual(["JC", "Leo"]);
         expect(result).toHaveLength(2);
     });
 });
--- src/wordTable.js
+++ src/wordTable.js
@@ -1,14 +1,11 @@
-
-
-function deleteWordWithX(array){
+function deleteWordWithX(array) {
     let table = [];
-    for(let i = 0; i < array.length; i++){
-        if(array[i].toLowerCase().indexOf('x') === -1){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].toLowerCase().indexOf("x") === -1) {
             table.push(array[i]);
         }
     }
     return table;
 }
-
 
 exports.deleteWordWithX = deleteWordWithX;
