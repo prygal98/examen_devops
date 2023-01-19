bb2ed68de331630c5c784865bfe054912d014a93
Raphael Malka <raphael.malka@student.vinci.be>
2023-01-19 19:07:42 +0100

    the tree test created

--- src/__tests__/wordTable.test.js
+++ src/__tests__/wordTable.test.js
@@ -18,3 +18,18 @@ describe("Second test to check if the function return the correct values ", () =
 });
 
 
+describe("Third test to check if the function return the correct values ", () => {
+    test("should return a table with the 2 elements", () => {
+        const result = deleteWordWithX(['DMX', 'Xzibit', 'JeanKevin']);
+        expect(result).toEqual(['JeanKevin']);
+        expect(result).toHaveLength(1);
+    });
+});
+
+describe("Fourth test to check if the function return the correct values", () => {
+    test("should return a table with the 1 element", () => {
+        const result = deleteWordWithX(['JC', 'Leo', 'xena']);
+        expect(result).toEqual(['JC','Leo']);
+        expect(result).toHaveLength(2);
+    });
+});
